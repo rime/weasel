2d04831f51a022408d9e856d456cb8f6f94d6c14
lotem <chen.sst@gmail.com>
2011-12-08 18:02:26 +0000

    Switcher: schema a la menu!
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/weasel@381 b90aed45-4297-9963-ff9d-98b604ac09a9

--- quick_build.bat
+++ quick_build.bat
@@ -4,8 +4,9 @@ call vcbuild.bat
 if errorlevel 1 goto error
 cd %work%
 output\weaselserver /q
-devenv weasel.sln /Build "Release"
-rem devenv weasel.sln /Build "ReleaseHant"
+del Release\RimeWithWeasel.lib
+devenv weasel.sln /Build "Release|Win32"
+rem devenv weasel.sln /Build "ReleaseHant|Win32"
 rem devenv weasel.sln /Build "Release|x64"
 rem devenv weasel.sln /Build "ReleaseHant|x64"
 if errorlevel 0 goto ok
