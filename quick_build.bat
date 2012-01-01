e2e5c356427a7d6b8173429696db9c56007ead47
lotem <chen.sst@gmail.com>
2012-01-01 13:09:01 +0000

    Weasel 0.9.3 release.
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/weasel@430 b90aed45-4297-9963-ff9d-98b604ac09a9

--- quick_build.bat
+++ quick_build.bat
@@ -1,15 +1,18 @@
 set work=%cd%
 cd %work%\..\librime
 call vcbuild.bat
-if errorlevel 1 goto error
 cd %work%
 output\weaselserver /q
+devenv weasel.sln /Build "ReleaseHant|x64"
+if errorlevel 1 goto error
+devenv weasel.sln /Build "ReleaseHant|Win32"
+if errorlevel 1 goto error
+devenv weasel.sln /Build "Release|x64"
+if errorlevel 1 goto error
 devenv weasel.sln /Build "Release|Win32"
-rem devenv weasel.sln /Build "ReleaseHant|Win32"
-rem devenv weasel.sln /Build "Release|x64"
-rem devenv weasel.sln /Build "ReleaseHant|x64"
-if errorlevel 0 goto ok
+if errorlevel 1 goto error
+goto end
 :error
 echo error building weasel...
-:ok
+:end
 cd %work%
