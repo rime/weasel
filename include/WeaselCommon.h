db873fc95bf63c3ca48993fa8dbe736dfa559945
GONG Chen <chen.sst@gmail.com>
2012-03-27 01:24:27 +0800

    Weasel 0.9.10.3.

--- include/WeaselCommon.h
+++ include/WeaselCommon.h
@@ -4,7 +4,7 @@
 #include <vector>
 
 #define WEASEL_CODE_NAME "Weasel"
-#define WEASEL_VERSION "0.9.10.2"
+#define WEASEL_VERSION "0.9.10.3"
 
 #define WEASEL_IME_NAME L"Ð¡ÀÇºÁ"
 #define WEASEL_IME_FILE L"weasel.ime"
