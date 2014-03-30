ee3494c729fb5de6c7fb1df314a106db28584fca
lotem <chen.sst@gmail.com>
2014-03-31 00:25:28 +0800

    Weasel 0.9.30 release.

--- include/WeaselVersion.h
+++ include/WeaselVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define WEASEL_VERSION_STR "0.9.29.1"
-#define WEASEL_VERSION_NUM 0,9,29,1
+#define WEASEL_VERSION_STR "0.9.30"
+#define WEASEL_VERSION_NUM 0,9,30,0
 
 #define WEASEL_CODE_NAME "Weasel"
 #define WEASEL_VERSION WEASEL_VERSION_STR
