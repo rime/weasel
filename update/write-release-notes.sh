4fd6a0c987f91ce21b66470d76df7c0b904f78a1
Chen Gong <chen.sst@gmail.com>
2018-03-14 23:51:20 +0800

    chore(update/write-release-notes.sh): add html/head/body tags

--- update/write-release-notes.sh
+++ update/write-release-notes.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# writes an HTML page for changes inr the latest release.
+# writes an HTML page for changes in the latest release
+
+cat <<EOF > release-notes.html
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+EOF
+
 # take advantage of the fact that change log generator
 # places 3 new lines after each chapter for a new release.
-
 cat CHANGELOG.md | awk '{
   print;
   if ($0 == "") ++new_lines; else new_lines = 0;
   if (new_lines >= 3) exit;
-}' | marked --gfm > release-notes.html
+}' | marked >> release-notes.html
+
+cat <<EOF >> release-notes.html
+</body>
+</html>
+EOF
