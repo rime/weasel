b0a5b33f59975369fe25792755c1603ecd83201e
Prcuvu <prcuvu@gmail.com>
2018-04-05 20:10:07 +0800

    feat(WeaselDeployer): detect registry value before running script

--- WeaselDeployer/SwitcherSettingsDialog.cpp
+++ WeaselDeployer/SwitcherSettingsDialog.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <set>
 #include <rime_levers_api.h>
+#include <WeaselUtility.h>
 
 
 SwitcherSettingsDialog::SwitcherSettingsDialog(RimeSwitcherSettings* settings)
@@ -97,7 +98,21 @@ LRESULT SwitcherSettingsDialog::OnClose(UINT, WPARAM, LPARAM, BOOL&) {
 }
 
 LRESULT SwitcherSettingsDialog::OnGetSchemata(WORD, WORD, HWND hWndCtl, BOOL&) {
-	ShellExecute(hWndCtl, _T("open"), _T("rime-install.bat"), NULL, NULL, SW_SHOW);
+	std::wstring parameter = std::wstring(L"/k (set plum_dir=") + WeaselUserDataPath() + L"\\plum) && (set git_mirror=taobao) && cd \"";
+	HKEY hKey;
+	LSTATUS ret = RegOpenKey(HKEY_LOCAL_MACHINE, _T("Software\\Rime\\Weasel"), &hKey);
+	if (ret == ERROR_SUCCESS) {
+		WCHAR value[MAX_PATH];
+		DWORD len = sizeof(value);
+		DWORD type = 0;
+		DWORD data = 0;
+		ret = RegQueryValueExW(hKey, L"WeaselRoot", NULL, &type, (LPBYTE)value, &len);
+		if (ret == ERROR_SUCCESS && type == REG_SZ)
+		{
+			ShellExecuteW(hWndCtl, L"open", L"cmd", (parameter + value + L"\" && rime-install.bat").c_str(), NULL, SW_SHOW);
+		}
+	}
+	RegCloseKey(hKey);
 	return 0;
 }
 
