6a6da469e3bdc14f49f29cf17a28b53d4ca6cd2a
nameoverflow <i@hcyue.me>
2018-11-12 19:50:36 +0800

    ci: download librime files from github releases

--- appveyor.install.bat
+++ appveyor.install.bat
@@ -8,8 +8,8 @@ if not exist %BOOST_ROOT% set nocache=1
 git submodule init
 git submodule update plum
 rem librime v1.3.0
-appveyor DownloadFile https://ci.appveyor.com/api/buildjobs/2gmg5qv6s2ykl04x/artifacts/rime.zip
-7z x rime.zip * -olibrime\ | find "ing archive"
+appveyor DownloadFile https://github.com/rime/librime/releases/download/1.3.2/rime-windows.zip
+7z x rime-windows.zip * -olibrime\ | find "ing archive"
 copy /Y librime\build\include\rime_*.h include\
 copy /Y librime\build\lib\Release\rime.dll output\
 
