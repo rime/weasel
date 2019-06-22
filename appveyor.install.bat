5e756d7f68ff9c64f444775ae402fa72e3e26951
Chen Gong <chen.sst@gmail.com>
2019-06-22 23:56:52 +0800

    refactor(appveyor): minimize invalidation of build cache

--- appveyor.install.bat
+++ appveyor.install.bat
@@ -1,36 +1,11 @@
 setlocal
 
-set boost_compiled_libs=--with-date_time^
- --with-filesystem^
- --with-locale^
- --with-regex^
- --with-serialization^
- --with-system^
- --with-thread
-
-set boost_build_options_common=toolset=msvc-14.1^
- variant=release^
- link=static^
- threading=multi^
- runtime-link=static^
- cxxflags="/Zc:threadSafeInit- "
-
-set boost_build_options_x86=%boost_build_options_common%^
- define=BOOST_USE_WINAPI_VERSION=0x0501
-
-set boost_build_options_x64=%boost_build_options_common%^
- define=BOOST_USE_WINAPI_VERSION=0x0502^
- address-model=64^
- --stagedir=stage_x64
-
-set nocache=0
-
-if not exist boost.cached set nocache=1
-if not exist %BOOST_ROOT%/stage set nocache=1
-if not exist %BOOST_ROOT%/stage_x64 set nocache=1
+call appveyor_build_boost.bat
+if %ERRORLEVEL% NEQ 0 goto ERROR
 
 git submodule init
 git submodule update plum
+
 set librime_version=1.5.3
 set download_archive=rime-with-plugins-%librime_version%-win32.zip
 appveyor DownloadFile https://github.com/rime/librime/releases/download/%librime_version%/%download_archive%
@@ -40,27 +15,6 @@ copy /Y librime\dist\lib\rime.lib lib\
 copy /Y librime\dist\lib\rime.dll output\
 if not exist output\data\opencc mkdir output\data\opencc
 copy /Y librime\thirdparty\share\opencc\*.* output\data\opencc\
-
-if %nocache% == 1 (
-	pushd %BOOST_ROOT%
-	call .\bootstrap.bat
-	.\b2.exe %boost_build_options_x86% %boost_compiled_libs% -q -d0 stage
-	.\b2.exe %boost_build_options_x64% %boost_compiled_libs% -q -d0 stage
-	popd
-	if %ERRORLEVEL% NEQ 0 goto ERROR
-
-	date /t > boost.cached & time /t >> boost.cached
-	echo.
-	echo Boost libraries installed.
-	echo.
-	goto EXIT
-) else (
-	echo.
-	echo Last build date of cache is
-	type boost.cached
-	echo.
-	goto EXIT
-)
 
 :ERROR
 set EXITCODE=%ERRORLEVEL%
