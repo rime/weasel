e940643225a4bae0bd2da5c80e7dea2a8f81116d
liangfx <fx.liang@outlook.com>
2022-11-18 12:53:00 +0800

    CI configure 1.  add CI by github 2.  remove appveyor CI

--- env.vs2022.bat
+++ env.vs2022.bat
@@ -7,9 +7,9 @@ if not defined BOOST_ROOT set BOOST_ROOT=%WEASEL_ROOT%\deps\boost_1_78_0
 
 rem OPTIONAL: architecture, Visual Studio version and platform toolset
 rem set ARCH=Win32
-set BJAM_TOOLSET=msvc-14.2
-set CMAKE_GENERATOR="Visual Studio 16 2019"
-set PLATFORM_TOOLSET=v142
+set BJAM_TOOLSET=msvc-14.3
+set CMAKE_GENERATOR="Visual Studio 17 2022"
+set PLATFORM_TOOLSET=v143
 
 rem OPTIONAL: path to additional build tools
 rem set DEVTOOLS_PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\Git\usr\bin;%ProgramFiles%\CMake\bin;C:\Python27;
