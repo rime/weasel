e67418fd3dea1e3eef2c2b16295fc8c1a1dcfb2a
fxliang <fx.liang@outlook.com>
2023-08-11 12:50:16 +0800

    WeaselDeployer:  multilingual, automatically simplified and traditional Chinese by LICD.

--- WeaselDeployer/DictManagementDialog.cpp
+++ WeaselDeployer/DictManagementDialog.cpp
@@ -3,6 +3,7 @@
 #include "Configurator.h"
 #include <WeaselUtility.h>
 #include <rime_api.h>
+#include "WeaselDeployer.h"
 
 DictManagementDialog::DictManagementDialog()
 {
@@ -50,7 +51,8 @@ LRESULT DictManagementDialog::OnClose(UINT, WPARAM, LPARAM, BOOL&) {
 LRESULT DictManagementDialog::OnBackup(WORD, WORD code, HWND, BOOL&) {
 	int sel = user_dict_list_.GetCurSel();
 	if (sel < 0 || sel >= user_dict_list_.GetCount()) {
-		MessageBox(L"請在左列選擇要導出的詞典名稱。", L":-(", MB_OK | MB_ICONINFORMATION);
+		//MessageBox(L"請在左列選擇要導出的詞典名稱。", L":-(", MB_OK | MB_ICONINFORMATION);
+		MSG_BY_IDS(IDS_STR_SEL_EXPORT_DICT_NAME, IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
 		return 0;
 	}
 	std::wstring path;
@@ -64,7 +66,8 @@ LRESULT DictManagementDialog::OnBackup(WORD, WORD code, HWND, BOOL&) {
 	if (_waccess_s(path.c_str(), 0) != 0 &&
 		!CreateDirectoryW(path.c_str(), NULL) &&
 		GetLastError() == ERROR_PATH_NOT_FOUND) {
-		MessageBox(L"未能完成導出操作。會不會是同步文件夾無法訪問？", L":-(", MB_OK | MB_ICONERROR);
+		//MessageBox(L"未能完成導出操作。會不會是同步文件夾無法訪問？", L":-(", MB_OK | MB_ICONERROR);
+		MSG_BY_IDS(IDS_STR_ERREXPORT_SYNC_UV, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		return 0;
 	}
 	WCHAR dict_name[100] = {0};
@@ -72,11 +75,13 @@ LRESULT DictManagementDialog::OnBackup(WORD, WORD code, HWND, BOOL&) {
 	path += std::wstring(L"\\") + dict_name + L".userdb.txt";
 	std::string dict_name_str = wstring_to_string(dict_name, CP_UTF8);
 	if (!api_->backup_user_dict(dict_name_str.c_str())) {
-		MessageBox(L"不知哪裏出錯了，未能完成導出操作。", L":-(", MB_OK | MB_ICONERROR);
+		//MessageBox(L"不知哪裏出錯了，未能完成導出操作。", L":-(", MB_OK | MB_ICONERROR);
+		MSG_BY_IDS(IDS_STR_ERR_EXPORT_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		return 0;
 	}
 	else if (_waccess(path.c_str(), 0) != 0) {
-		MessageBox(L"咦，輸出的快照文件找不着了。", L":-(", MB_OK | MB_ICONERROR);
+		//MessageBox(L"咦，輸出的快照文件找不着了。", L":-(", MB_OK | MB_ICONERROR);
+		MSG_BY_IDS(IDS_STR_ERR_EXPORT_SNAP_LOST, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		return 0;
 	}
 	std::wstring param = L"/select, \"" + path + L"\"";
@@ -91,10 +96,12 @@ LRESULT DictManagementDialog::OnRestore(WORD, WORD code, HWND, BOOL&) {
 		char path[MAX_PATH] = {0};
 		WideCharToMultiByte(CP_ACP, 0, dlg.m_szFileName, -1, path, _countof(path), NULL, NULL);
 		if (!api_->restore_user_dict(path)) {
-			MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK | MB_ICONERROR);
+			//MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK | MB_ICONERROR);
+			MSG_BY_IDS(IDS_STR_ERR_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		}
 		else {
-			MessageBox(L"完成了。", L":-)", MB_OK | MB_ICONINFORMATION);
+			//MessageBox(L"完成了。", L":-)", MB_OK | MB_ICONINFORMATION);
+			MSG_BY_IDS(IDS_STR_ERR_SUCCESS, IDS_STR_HAPPY, MB_OK | MB_ICONINFORMATION);
 		}
 	}
 	return 0;
@@ -103,7 +110,8 @@ LRESULT DictManagementDialog::OnRestore(WORD, WORD code, HWND, BOOL&) {
 LRESULT DictManagementDialog::OnExport(WORD, WORD code, HWND, BOOL&) {
 	int sel = user_dict_list_.GetCurSel();
 	if (sel < 0 || sel >= user_dict_list_.GetCount()) {
-		MessageBox(L"請在左列選擇要導出的詞典名稱。", L":-(", MB_OK | MB_ICONINFORMATION);
+		//MessageBox(L"請在左列選擇要導出的詞典名稱。", L":-(", MB_OK | MB_ICONINFORMATION);
+		MSG_BY_IDS(IDS_STR_SEL_EXPORT_DICT_NAME, IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
 		return 0;
 	}
 	WCHAR dict_name[MAX_PATH] = {0};
@@ -117,14 +125,17 @@ LRESULT DictManagementDialog::OnExport(WORD, WORD code, HWND, BOOL&) {
 		std::string dict_name_str = wstring_to_string(dict_name, CP_UTF8);
 		int result = api_->export_user_dict(dict_name_str.c_str(), path);
 		if (result < 0) {
-			MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK | MB_ICONERROR);
+			//MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK | MB_ICONERROR);
+			MSG_BY_IDS(IDS_STR_ERR_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		}
 		else if (_waccess(dlg.m_szFileName, 0) != 0) {
-			MessageBox(L"咦，導出的文件找不着了。", L":-(", MB_OK | MB_ICONERROR);
+			//MessageBox(L"咦，導出的文件找不着了。", L":-(", MB_OK | MB_ICONERROR);
+			MSG_BY_IDS(IDS_STR_ERR_EXPORT_FILE_LOST, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		}
 		else {
 			std::wstring report(L"導出了 " + std::to_wstring(result) + L" 條記錄。");
-			MessageBox(report.c_str(), L":-)", MB_OK | MB_ICONINFORMATION);
+			//MessageBox(report.c_str(), L":-)", MB_OK | MB_ICONINFORMATION);
+			MSG_ID_CAP(report.c_str(), IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
 			std::wstring param = L"/select, \"" + std::wstring(dlg.m_szFileName) + L"\"";
 			ShellExecute(NULL, L"open", L"explorer.exe", param.c_str(), NULL, SW_SHOWNORMAL);
 		}
@@ -135,7 +146,8 @@ LRESULT DictManagementDialog::OnExport(WORD, WORD code, HWND, BOOL&) {
 LRESULT DictManagementDialog::OnImport(WORD, WORD code, HWND, BOOL&) {
 	int sel = user_dict_list_.GetCurSel();
 	if (sel < 0 || sel >= user_dict_list_.GetCount()) {
-		MessageBox(L"請在左列選擇要導入的詞典名稱。", L":-(", MB_OK | MB_ICONINFORMATION);
+		//MessageBox(L"請在左列選擇要導入的詞典名稱。", L":-(", MB_OK | MB_ICONINFORMATION);
+		MSG_BY_IDS(IDS_STR_SEL_IMPORT_DICT_NAME, IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
 		return 0;
 	}
 	WCHAR dict_name[MAX_PATH] = {0};
@@ -148,11 +160,13 @@ LRESULT DictManagementDialog::OnImport(WORD, WORD code, HWND, BOOL&) {
 		WideCharToMultiByte(CP_ACP, 0, dlg.m_szFileName, -1, path, _countof(path), NULL, NULL);
 		int result = api_->import_user_dict(wstring_to_string(dict_name, CP_UTF8).c_str(), path);
 		if (result < 0) {
-			MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK | MB_ICONERROR);
+			//MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK | MB_ICONERROR);
+			MSG_BY_IDS(IDS_STR_ERR_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
 		}
 		else {
 			std::wstring report(L"導入了 " + std::to_wstring(result) + L" 條記錄。");
-			MessageBox(report.c_str(), L":-)", MB_OK | MB_ICONINFORMATION);
+			//MessageBox(report.c_str(), L":-)", MB_OK | MB_ICONINFORMATION);
+			MSG_ID_CAP(report.c_str(), IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
 		}
 	}
 	return 0;
--- WeaselDeployer/WeaselDeployer.h
+++ WeaselDeployer/WeaselDeployer.h
@@ -2,3 +2,24 @@
 
 #include "resource.h"
 #include <string>
+#include <atlstr.h>
+#include <Windows.h>
+#include "resource.h"
+
+#define MSG_BY_IDS(idInfo, idCap, uType) \
+	{\
+		CString info, cap;\
+		info.LoadStringW(idInfo);\
+		cap.LoadStringW(idCap);\
+		LANGID langID = GetThreadUILanguage();\
+		MessageBoxExW(NULL, info, cap, uType, langID);\
+	}
+
+#define MSG_ID_CAP(info, idCap, uType) \
+	{\
+		CString cap;\
+		cap.LoadStringW(idCap);\
+		LANGID langID = GetThreadUILanguage();\
+		MessageBoxExW(NULL, info, cap, uType, langID);\
+	}
+
