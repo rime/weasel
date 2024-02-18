77b6af2e9658430022223d32079dfbbcf6de631b
fxliang <fx.liang@outlook.com>
2024-02-18 16:29:04 +0800

    fix ci

--- env.vs2022.bat
+++ env.vs2022.bat
@@ -3,10 +3,10 @@ rem Customize your build environment and save the modified copy to env.bat
 set WEASEL_ROOT=%CD%
 
 rem REQUIRED: path to Boost source directory
-if not defined BOOST_ROOT set BOOST_ROOT=%WEASEL_ROOT%\deps\boost_1_78_0
+if not defined BOOST_ROOT set BOOST_ROOT=%WEASEL_ROOT%\deps\boost_1_83_0
 
 rem OPTIONAL: architecture, Visual Studio version and platform toolset
-set ARCH=Win32
+if not defined ARCH set ARCH=Win32
 set BJAM_TOOLSET=msvc-14.3
 set CMAKE_GENERATOR="Visual Studio 17 2022"
 set PLATFORM_TOOLSET=v143
