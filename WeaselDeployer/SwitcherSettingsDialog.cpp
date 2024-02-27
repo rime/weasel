d918ace06498c9ade0a071f7967ca64e5077502b
fxliang <fx.liang@outlook.com>
2024-02-27 11:43:39 +0800

    refactor: fixed DWORD ipc_id (combination of pid and order_id) instead of RimeSessionId build: build 32/64 bit WeaselServer and WeaselDeployer, default x64 build in `output`, Win32 build in `output\Win32` update WeaselServerApp.h: de-warning for 64 bit build. chore：TestWeaselIPC with a lowercase 'a' instead of 'A', for more meaningful actions lib: update WinSparkle to 0.8.1

--- WeaselDeployer/SwitcherSettingsDialog.cpp
+++ WeaselDeployer/SwitcherSettingsDialog.cpp
@@ -104,10 +104,25 @@ LRESULT SwitcherSettingsDialog::OnClose(UINT, WPARAM, LPARAM, BOOL&) {
   return 0;
 }
 
+BOOL is_wow64() {
+  DWORD errorCode;
+  if (GetSystemWow64DirectoryW(NULL, 0) == 0)
+    if ((errorCode = GetLastError()) == ERROR_CALL_NOT_IMPLEMENTED)
+      return FALSE;
+    else
+      ExitProcess((UINT)errorCode);
+  else
+    return TRUE;
+}
+
 LRESULT SwitcherSettingsDialog::OnGetSchemata(WORD, WORD, HWND hWndCtl, BOOL&) {
   HKEY hKey;
-  LSTATUS ret =
-      RegOpenKey(HKEY_LOCAL_MACHINE, _T("Software\\Rime\\Weasel"), &hKey);
+  std::wstring hPath;
+  if (is_wow64())
+    hPath = _T("Software\\WOW6432Node\\Rime\\Weasel");
+  else
+    hPath = _T("Software\\Rime\\Weasel");
+  LSTATUS ret = RegOpenKey(HKEY_LOCAL_MACHINE, hPath.c_str(), &hKey);
   if (ret == ERROR_SUCCESS) {
     WCHAR value[MAX_PATH];
     DWORD len = sizeof(value);
