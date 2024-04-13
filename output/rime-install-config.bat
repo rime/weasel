4842f095ba5b53be7e72c2bbe52b1638e29cc2c7
居戎氏 <chen.sst@gmail.com>
2024-04-13 11:09:05 +0800

    update output/rime-install-config.bat

--- output/rime-install-config.bat
+++ output/rime-install-config.bat
@@ -1,5 +1,19 @@
-rem for downloading Git for Windows installer
-set git_mirror=taobao
+rem Location of download cache
+rem set download_cache_dir=%TEMP%
+
+rem Do not update packages; only download missing files.
+rem CAUTION: may suffer from incomplete downloads.
+rem set no_update=1
 
 rem Rime configuration manager and downloaded packages
 set plum_dir=%APPDATA%\plum
+
+rem Location of Rime user directory
+rem set rime_dir=%APPDATA%\Rime
+
+set key=HKEY_CURRENT_USER\SOFTWARE\Rime\Weasel
+set name=RimeUserDir
+for /f "tokens=2*" %%a in ('reg query "%key%" /v "%name%"') do set rime_dir=%%b
+
+rem Disable /plum/ bash script; use batch installer only.
+rem set use_plum=0
