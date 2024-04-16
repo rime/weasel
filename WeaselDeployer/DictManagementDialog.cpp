057164ff0f161a73093a6c89f53fa57b8e729368
cc <ccyybn@gmail.com>
2024-04-16 15:34:45 +0800

    Fix the multiple explorer processes appearing after exporting a dict file, updating the file dialog box to win7 style.

--- WeaselDeployer/DictManagementDialog.cpp
+++ WeaselDeployer/DictManagementDialog.cpp
@@ -4,6 +4,70 @@
 #include <WeaselUtility.h>
 #include <rime_api.h>
 #include "WeaselDeployer.h"
+#include <regex>
+
+void static OpenFolderAndSelectItem(std::wstring filepath) {
+  filepath = std::regex_replace(filepath, std::wregex(L"/"), L"\\");
+  std::wstring directory;
+  const size_t last_slash_idx = filepath.rfind('\\');
+  if (std::string::npos != last_slash_idx) {
+    directory = filepath.substr(0, last_slash_idx);
+  }
+
+  HRESULT hr;
+  hr = CoInitializeEx(0, COINIT_MULTITHREADED);
+
+  ITEMIDLIST* folder = ILCreateFromPath(directory.c_str());
+  std::vector<LPITEMIDLIST> v;
+  v.push_back(ILCreateFromPath(filepath.c_str()));
+
+  SHOpenFolderAndSelectItems(folder, v.size(), (LPCITEMIDLIST*)v.data(), 0);
+
+  for (auto idl : v) {
+    ILFree(idl);
+  }
+  ILFree(folder);
+}
+
+static wchar_t* DoFileDialog(HWND hwndOwner,
+                             BOOL bOpenFileDialog,
+                             LPCWSTR title,
+                             UINT size,
+                             COMDLG_FILTERSPEC filter[],
+                             LPCWSTR filename,
+                             LPCWSTR defExt) {
+  IFileDialog* pfd = NULL;
+  IShellItem* psi = NULL;
+  wchar_t* path = NULL;
+  HRESULT hr = NULL;
+
+  CoInitialize(NULL);
+  if (bOpenFileDialog) {
+    hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER,
+                          IID_IFileOpenDialog, (void**)(&pfd));
+  } else {
+    hr = CoCreateInstance(CLSID_FileSaveDialog, NULL, CLSCTX_INPROC_SERVER,
+                          IID_IFileSaveDialog, (void**)(&pfd));
+  }
+  hr = pfd->SetFileTypes(size, filter);
+  hr = pfd->SetTitle(title);
+  if (filename != NULL) {
+    hr = pfd->SetFileName(filename);
+  }
+  hr = pfd->SetDefaultExtension(defExt);
+  hr = pfd->Show(hwndOwner);
+  hr = pfd->GetResult(&psi);
+  if (SUCCEEDED(hr)) {
+    hr = psi->GetDisplayName(SIGDN_FILESYSPATH, &path);
+    if (SUCCEEDED(hr)) {
+      return path;
+    }
+    psi->Release();
+  }
+  pfd->Release();
+  CoUninitialize();
+  return NULL;
+}
 
 DictManagementDialog::DictManagementDialog() {
   api_ = (RimeLeversApi*)rime_get_api()->find_module("levers")->get_api();
@@ -48,8 +112,6 @@ LRESULT DictManagementDialog::OnClose(UINT, WPARAM, LPARAM, BOOL&) {
 LRESULT DictManagementDialog::OnBackup(WORD, WORD code, HWND, BOOL&) {
   int sel = user_dict_list_.GetCurSel();
   if (sel < 0 || sel >= user_dict_list_.GetCount()) {
-    // MessageBox(L"請在左列選擇要導出的詞典名稱。", L":-(", MB_OK |
-    // MB_ICONINFORMATION);
     MSG_BY_IDS(IDS_STR_SEL_EXPORT_DICT_NAME, IDS_STR_SAD,
                MB_OK | MB_ICONINFORMATION);
     return 0;
@@ -65,8 +127,6 @@ LRESULT DictManagementDialog::OnBackup(WORD, WORD code, HWND, BOOL&) {
   if (_waccess_s(path.c_str(), 0) != 0 &&
       !CreateDirectoryW(path.c_str(), NULL) &&
       GetLastError() == ERROR_PATH_NOT_FOUND) {
-    // MessageBox(L"未能完成導出操作。會不會是同步文件夾無法訪問？", L":-(",
-    // MB_OK | MB_ICONERROR);
     MSG_BY_IDS(IDS_STR_ERREXPORT_SYNC_UV, IDS_STR_SAD, MB_OK | MB_ICONERROR);
     return 0;
   }
@@ -75,39 +135,36 @@ LRESULT DictManagementDialog::OnBackup(WORD, WORD code, HWND, BOOL&) {
   path += std::wstring(L"\\") + dict_name + L".userdb.txt";
   std::string dict_name_str = wstring_to_string(dict_name, CP_UTF8);
   if (!api_->backup_user_dict(dict_name_str.c_str())) {
-    // MessageBox(L"不知哪裏出錯了，未能完成導出操作。", L":-(", MB_OK |
-    // MB_ICONERROR);
     MSG_BY_IDS(IDS_STR_ERR_EXPORT_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
     return 0;
   } else if (_waccess(path.c_str(), 0) != 0) {
-    // MessageBox(L"咦，輸出的快照文件找不着了。", L":-(", MB_OK |
-    // MB_ICONERROR);
     MSG_BY_IDS(IDS_STR_ERR_EXPORT_SNAP_LOST, IDS_STR_SAD, MB_OK | MB_ICONERROR);
     return 0;
   }
-  std::wstring param = L"/select, \"" + path + L"\"";
-  ShellExecute(NULL, L"open", L"explorer.exe", param.c_str(), NULL,
-               SW_SHOWNORMAL);
+  OpenFolderAndSelectItem(path);
   return 0;
 }
 
 LRESULT DictManagementDialog::OnRestore(WORD, WORD code, HWND, BOOL&) {
-  CFileDialog dlg(TRUE, L"snapshot", NULL, OFN_HIDEREADONLY | OFN_FILEMUSTEXIST,
-                  L"詞典快照\0*.userdb.txt\0KCSS格式詞典快照\0*.userdb.kct."
-                  L"snapshot\0全部文件\0*.*\0");
-  if (IDOK == dlg.DoModal()) {
+  COMDLG_FILTERSPEC filter[3] = {{L"詞典快照", L"*.userdb.txt"},
+                                 {L"KCSS格式詞典快照",
+                                  L"*.userdb.kct."
+                                  L"snapshot"},
+                                 {L"全部文件", L"*.*"}};
+
+  wchar_t* selectedPath = DoFileDialog(m_hWnd, TRUE, L"導入", ARRAYSIZE(filter),
+                                       filter, NULL, L"snapshot");
+  if (selectedPath != NULL) {
     char path[MAX_PATH] = {0};
-    WideCharToMultiByte(CP_ACP, 0, dlg.m_szFileName, -1, path, _countof(path),
-                        NULL, NULL);
+    WideCharToMultiByte(CP_ACP, 0, selectedPath, -1, path, _countof(path), NULL,
+                        NULL);
     if (!api_->restore_user_dict(path)) {
-      // MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK |
-      // MB_ICONERROR);
       MSG_BY_IDS(IDS_STR_ERR_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
     } else {
-      // MessageBox(L"完成了。", L":-)", MB_OK | MB_ICONINFORMATION);
       MSG_BY_IDS(IDS_STR_ERR_SUCCESS, IDS_STR_HAPPY,
                  MB_OK | MB_ICONINFORMATION);
     }
+    CoTaskMemFree(selectedPath);
   }
   return 0;
 }
@@ -115,8 +172,6 @@ LRESULT DictManagementDialog::OnRestore(WORD, WORD code, HWND, BOOL&) {
 LRESULT DictManagementDialog::OnExport(WORD, WORD code, HWND, BOOL&) {
   int sel = user_dict_list_.GetCurSel();
   if (sel < 0 || sel >= user_dict_list_.GetCount()) {
-    // MessageBox(L"請在左列選擇要導出的詞典名稱。", L":-(", MB_OK |
-    // MB_ICONINFORMATION);
     MSG_BY_IDS(IDS_STR_SEL_EXPORT_DICT_NAME, IDS_STR_SAD,
                MB_OK | MB_ICONINFORMATION);
     return 0;
@@ -125,32 +180,30 @@ LRESULT DictManagementDialog::OnExport(WORD, WORD code, HWND, BOOL&) {
   user_dict_list_.GetText(sel, dict_name);
   std::wstring file_name(dict_name);
   file_name += L"_export.txt";
-  CFileDialog dlg(FALSE, L"txt", file_name.c_str(),
-                  OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT,
-                  L"文本文檔\0*.txt\0全部文件\0*.*\0");
-  if (IDOK == dlg.DoModal()) {
+
+  COMDLG_FILTERSPEC filter[2] = {{L"文本文檔", L"*.txt"},
+                                 {L"全部文件", L"*.*"}};
+
+  wchar_t* selectedPath =
+      DoFileDialog(m_hWnd, FALSE, L"導出", ARRAYSIZE(filter), filter,
+                   file_name.c_str(), L"txt");
+  if (selectedPath != NULL) {
     char path[MAX_PATH] = {0};
-    WideCharToMultiByte(CP_ACP, 0, dlg.m_szFileName, -1, path, _countof(path),
-                        NULL, NULL);
+    WideCharToMultiByte(CP_ACP, 0, selectedPath, -1, path, _countof(path), NULL,
+                        NULL);
     std::string dict_name_str = wstring_to_string(dict_name, CP_UTF8);
     int result = api_->export_user_dict(dict_name_str.c_str(), path);
     if (result < 0) {
-      // MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK |
-      // MB_ICONERROR);
       MSG_BY_IDS(IDS_STR_ERR_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
-    } else if (_waccess(dlg.m_szFileName, 0) != 0) {
-      // MessageBox(L"咦，導出的文件找不着了。", L":-(", MB_OK | MB_ICONERROR);
+    } else if (_waccess(selectedPath, 0) != 0) {
       MSG_BY_IDS(IDS_STR_ERR_EXPORT_FILE_LOST, IDS_STR_SAD,
                  MB_OK | MB_ICONERROR);
     } else {
       std::wstring report(L"導出了 " + std::to_wstring(result) + L" 條記錄。");
-      // MessageBox(report.c_str(), L":-)", MB_OK | MB_ICONINFORMATION);
       MSG_ID_CAP(report.c_str(), IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
-      std::wstring param =
-          L"/select, \"" + std::wstring(dlg.m_szFileName) + L"\"";
-      ShellExecute(NULL, L"open", L"explorer.exe", param.c_str(), NULL,
-                   SW_SHOWNORMAL);
+      OpenFolderAndSelectItem(selectedPath);
     }
+    CoTaskMemFree(selectedPath);
   }
   return 0;
 }
@@ -158,8 +211,6 @@ LRESULT DictManagementDialog::OnExport(WORD, WORD code, HWND, BOOL&) {
 LRESULT DictManagementDialog::OnImport(WORD, WORD code, HWND, BOOL&) {
   int sel = user_dict_list_.GetCurSel();
   if (sel < 0 || sel >= user_dict_list_.GetCount()) {
-    // MessageBox(L"請在左列選擇要導入的詞典名稱。", L":-(", MB_OK |
-    // MB_ICONINFORMATION);
     MSG_BY_IDS(IDS_STR_SEL_IMPORT_DICT_NAME, IDS_STR_SAD,
                MB_OK | MB_ICONINFORMATION);
     return 0;
@@ -168,24 +219,25 @@ LRESULT DictManagementDialog::OnImport(WORD, WORD code, HWND, BOOL&) {
   user_dict_list_.GetText(sel, dict_name);
   std::wstring file_name(dict_name);
   file_name += L"_export.txt";
-  CFileDialog dlg(TRUE, L"txt", file_name.c_str(),
-                  OFN_HIDEREADONLY | OFN_FILEMUSTEXIST,
-                  L"文本文檔\0*.txt\0全部文件\0*.*\0");
-  if (IDOK == dlg.DoModal()) {
+
+  COMDLG_FILTERSPEC filter[2] = {{L"文本文檔", L"*.txt"},
+                                 {L"全部文件", L"*.*"}};
+
+  wchar_t* selectedPath = DoFileDialog(m_hWnd, TRUE, L"導入", ARRAYSIZE(filter),
+                                       filter, file_name.c_str(), L"txt");
+  if (selectedPath != NULL) {
     char path[MAX_PATH] = {0};
-    WideCharToMultiByte(CP_ACP, 0, dlg.m_szFileName, -1, path, _countof(path),
-                        NULL, NULL);
+    WideCharToMultiByte(CP_ACP, 0, selectedPath, -1, path, _countof(path), NULL,
+                        NULL);
     int result = api_->import_user_dict(
         wstring_to_string(dict_name, CP_UTF8).c_str(), path);
     if (result < 0) {
-      // MessageBox(L"不知哪裏出錯了，未能完成操作。", L":-(", MB_OK |
-      // MB_ICONERROR);
       MSG_BY_IDS(IDS_STR_ERR_UNKNOWN, IDS_STR_SAD, MB_OK | MB_ICONERROR);
     } else {
       std::wstring report(L"導入了 " + std::to_wstring(result) + L" 條記錄。");
-      // MessageBox(report.c_str(), L":-)", MB_OK | MB_ICONINFORMATION);
       MSG_ID_CAP(report.c_str(), IDS_STR_SAD, MB_OK | MB_ICONINFORMATION);
     }
+    CoTaskMemFree(selectedPath);
   }
   return 0;
 }
