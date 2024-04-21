98fe8099d846d3fee9aed2b0159593aaa29dff68
fxliang <fx.liang@outlook.com>
2024-04-21 13:26:12 +0800

    remove unnecessary regex

--- WeaselDeployer/DictManagementDialog.cpp
+++ WeaselDeployer/DictManagementDialog.cpp
@@ -4,11 +4,10 @@
 #include <WeaselUtility.h>
 #include <rime_api.h>
 #include "WeaselDeployer.h"
-#include <regex>
 
 void static OpenFolderAndSelectItem(std::wstring filepath) {
-  filepath = std::regex_replace(filepath, std::wregex(L"/"), L"\\");
-  std::wstring directory = std::filesystem::path(filepath).parent_path();
+  std::wstring directory =
+      std::filesystem::path(filepath).parent_path().wstring();
 
   HRESULT hr;
   hr = CoInitializeEx(0, COINIT_MULTITHREADED);
