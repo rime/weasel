a53fd0c0e02edce336bccff641fa16bb673a86ec
居戎氏 <chen.sst@gmail.com>
2024-05-14 00:30:57 +0800

    chore(release): 0.16.0 :tada:

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ if exist env.bat call env.bat
 if not defined WEASEL_ROOT set WEASEL_ROOT=%CD%
 
 if not defined VERSION_MAJOR set VERSION_MAJOR=0
-if not defined VERSION_MINOR set VERSION_MINOR=15
+if not defined VERSION_MINOR set VERSION_MINOR=16
 if not defined VERSION_PATCH set VERSION_PATCH=0
 
 if not defined WEASEL_VERSION set WEASEL_VERSION=%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH%
