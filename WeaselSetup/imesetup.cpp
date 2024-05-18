957a8e30542af8211ee305e26145351e7e3c07b0
fxliang <fx.liang@outlook.com>
2024-05-18 12:36:15 +0800

    feat: enable/disable Windows Error Report when installed/uninstalled

--- WeaselSetup/imesetup.cpp
+++ WeaselSetup/imesetup.cpp
@@ -544,6 +544,11 @@ int register_text_service(const std::wstring& tsf_path,
   return 0;
 }
 
+void executeCommandA(const std::string& command) {
+  ShellExecuteA(NULL, "open", "cmd.exe", ("/C " + command).c_str(), NULL,
+                SW_HIDE);
+}
+
 int install(bool hant, bool silent, bool old_ime_support) {
   std::wstring ime_src_path;
   int retval = 0;
@@ -608,6 +613,51 @@ int install(bool hant, bool silent, bool old_ime_support) {
     FreeLibrary(hInputDLL);
   }
 
+  // write WER register
+  if (is_wow64()) {
+    PVOID OldValue = NULL;
+    if (Wow64DisableWow64FsRedirection(&OldValue) == FALSE) {
+      MSG_NOT_SILENT_BY_IDS(silent, IDS_STR_ERRCANCELFSREDIRECT,
+                            IDS_STR_UNINSTALL_FAILED, MB_ICONERROR | MB_OK);
+      return 1;
+    }
+  }
+  // https://learn.microsoft.com/zh-cn/windows/win32/wer/collecting-user-mode-dumps
+  const std::string dmpPath = WeaselLogPath().string();
+  executeCommandA(
+      "REG ADD \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows "
+      "Error Reporting\\LocalDumps\\WeaselServer.exe\" /f");
+  // dump file path
+  executeCommandA(
+      "REG ADD \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows "
+      "Error Reporting\\LocalDumps\\WeaselServer.exe\" /t REG_SZ /v DumpFolder "
+      "/d " +
+      dmpPath + " /f");
+  // dump type 0
+  executeCommandA(
+      "REG ADD \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows "
+      "Error Reporting\\LocalDumps\\WeaselServer.exe\" /t REG_DWORD /v "
+      "DumpType /d 0 /f");
+  // CustomDumpFlags, MiniDumpNormal
+  executeCommandA(
+      "REG ADD \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows "
+      "Error Reporting\\LocalDumps\\WeaselServer.exe\" /t REG_DWORD /v "
+      "CustomDumpFlags /d 0 /f");
+  // maximium dump count 10
+  executeCommandA(
+      "REG ADD \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows "
+      "Error Reporting\\LocalDumps\\WeaselServer.exe\" /t REG_DWORD /v "
+      "DumpCount /d 10 /f");
+
+  if (is_wow64()) {
+    PVOID OldValue = NULL;
+    if (Wow64RevertWow64FsRedirection(&OldValue) == FALSE) {
+      MSG_NOT_SILENT_BY_IDS(silent, IDS_STR_ERRRECOVERFSREDIRECT,
+                            IDS_STR_UNINSTALL_FAILED, MB_ICONERROR | MB_OK);
+      return 1;
+    }
+  }
+
   if (retval)
     return 1;
 
@@ -655,6 +705,26 @@ int uninstall(bool silent) {
   RegDeleteKey(HKEY_LOCAL_MACHINE, WEASEL_REG_KEY);
   RegDeleteKey(HKEY_LOCAL_MACHINE, RIME_REG_KEY);
 
+  // delete WER register
+  if (is_wow64()) {
+    PVOID OldValue = NULL;
+    if (Wow64DisableWow64FsRedirection(&OldValue) == FALSE) {
+      MSG_NOT_SILENT_BY_IDS(silent, IDS_STR_ERRCANCELFSREDIRECT,
+                            IDS_STR_UNINSTALL_FAILED, MB_ICONERROR | MB_OK);
+      return 1;
+    }
+  }
+  executeCommandA(
+      "REG DELETE \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows "
+      "Error Reporting\\LocalDumps\\WeaselServer.exe\"  /f");
+  if (is_wow64()) {
+    PVOID OldValue = NULL;
+    if (Wow64RevertWow64FsRedirection(&OldValue) == FALSE) {
+      MSG_NOT_SILENT_BY_IDS(silent, IDS_STR_ERRRECOVERFSREDIRECT,
+                            IDS_STR_UNINSTALL_FAILED, MB_ICONERROR | MB_OK);
+      return 1;
+    }
+  }
   if (retval)
     return 1;
 
