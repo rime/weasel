rime/weasel

commit 39713c987c71fd0b9dc626ba7aa4a9004c57a6f0

improvement: set log_dir in %TEMP%\rime.weasel, for easier exploring logs

RimeWithWeasel/RimeWithWeasel.cpp
@@ -100,6 +100,7 @@ void RimeWithWeaselHandler::_Setup() {
   weasel_traits.distribution_code_name = WEASEL_CODE_NAME;
   weasel_traits.distribution_version = WEASEL_VERSION;
   weasel_traits.app_name = "rime.weasel";
+  weasel_traits.log_dir = WeaselLogPath().u8string().c_str();
   RimeSetup(&weasel_traits);
   RimeSetNotificationHandler(&RimeWithWeaselHandler::OnNotify, this);
 }

WeaselDeployer/Configurator.cpp
@@ -46,6 +46,7 @@ void Configurator::Initialize() {
   weasel_traits.distribution_code_name = WEASEL_CODE_NAME;
   weasel_traits.distribution_version = WEASEL_VERSION;
   weasel_traits.app_name = "rime.weasel";
+  weasel_traits.log_dir = WeaselLogPath().u8string().c_str();
   RimeSetup(&weasel_traits);
 
   LOG(INFO) << "WeaselDeployer reporting.";

RimeWithWeasel/WeaselUtility.cpp
@@ -3,8 +3,6 @@
 #include <string>
 #include <WeaselUtility.h>
 
-namespace fs = std::filesystem;
-
 fs::path WeaselUserDataPath() {
   WCHAR _path[MAX_PATH] = {0};
   const WCHAR KEY[] = L"Software\\Rime\\Weasel";

WeaselServer/WeaselServerApp.cpp
@@ -63,9 +63,6 @@ void WeaselServerApp::SetupMenuHandlers() {
   m_server.AddMenuHandler(ID_WEASELTRAY_INSTALLDIR, std::bind(explore, dir));
   m_server.AddMenuHandler(ID_WEASELTRAY_USERCONFIG,
                           std::bind(explore, WeaselUserDataPath()));
-  WCHAR _logDir[MAX_PATH];
-  // in the future if log dir customizable, to be changed
-  ExpandEnvironmentStringsW(L"%TEMP%", _logDir, _countof(_logDir));
   m_server.AddMenuHandler(ID_WEASELTRAY_LOGDIR,
-                          std::bind(explore, std::filesystem::path(_logDir)));
+                          std::bind(explore, WeaselLogPath()));
 }

WeaselTSF/LanguageBar.cpp
@@ -330,10 +330,7 @@ void WeaselTSF::_HandleLangBarMenuSelect(UINT wID) {
       }
       break;
     case ID_WEASELTRAY_LOGDIR:
-      WCHAR _logDir[MAX_PATH];
-      // in the future if log dir customizable, to be changed
-      ExpandEnvironmentStringsW(L"%TEMP%", _logDir, _countof(_logDir));
-      explore(_logDir);
+      explore(WeaselLogPath().wstring());
       break;
     case ID_WEASELTRAY_WIKI:
       open(L"https://rime.im/docs/");

include/WeaselUtility.h
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include <string>
 
+namespace fs = std::filesystem;
+
 inline int utf8towcslen(const char* utf8_str, int utf8_len) {
   return MultiByteToWideChar(CP_UTF8, 0, utf8_str, utf8_len, NULL, 0);
 }
@@ -29,6 +31,16 @@ inline std::wstring getUsername() {
 // data directories
 std::filesystem::path WeaselSharedDataPath();
 std::filesystem::path WeaselUserDataPath();
+inline fs::path WeaselLogPath() {
+  WCHAR _path[MAX_PATH] = {0};
+  // default location
+  ExpandEnvironmentStringsW(L"%TEMP%\\rime.weasel", _path, _countof(_path));
+  fs::path path = fs::path(_path);
+  if (!fs::exists(path)) {
+    fs::create_directories(path);
+  }
+  return path;
+}
 
 inline BOOL IsUserDarkMode() {
   constexpr const LPCWSTR key =

commit 8246b8c511d19a788935ee0b1e295fecc34a78c6

fix: log dir compiling issue introduced by 39713c9 - not log files sometime - can not restart service sometime - can not restart service with running WeaselServer.exe

RimeWithWeasel/RimeWithWeasel.cpp
@@ -101,7 +101,8 @@ void RimeWithWeaselHandler::_Setup() {
   weasel_traits.distribution_code_name = WEASEL_CODE_NAME;
   weasel_traits.distribution_version = WEASEL_VERSION;
   weasel_traits.app_name = "rime.weasel";
-  weasel_traits.log_dir = WeaselLogPath().u8string().c_str();
+  std::string log_dir = WeaselLogPath().u8string();
+  weasel_traits.log_dir = log_dir.c_str();
   RimeSetup(&weasel_traits);
   RimeSetNotificationHandler(&RimeWithWeaselHandler::OnNotify, this);
 }

WeaselDeployer/Configurator.cpp
@@ -47,7 +47,8 @@ void Configurator::Initialize() {
   weasel_traits.distribution_code_name = WEASEL_CODE_NAME;
   weasel_traits.distribution_version = WEASEL_VERSION;
   weasel_traits.app_name = "rime.weasel";
-  weasel_traits.log_dir = WeaselLogPath().u8string().c_str();
+  std::string log_dir = WeaselLogPath().u8string();
+  weasel_traits.log_dir = log_dir.c_str();
   RimeSetup(&weasel_traits);
 
   LOG(INFO) << "WeaselDeployer reporting.";