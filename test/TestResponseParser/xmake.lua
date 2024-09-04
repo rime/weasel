9275283bac7004837c943fe06cc83bc2659e1285
fxliang <fx.liang@outlook.com>
2024-09-04 21:39:59 +0800

    build: xmake optional (#1360)
    * build: xmake is now optional with xbuild.bat
    
    * chore: update llvm version in install-clang-format.bat

---
+++ test/TestResponseParser/xmake.lua
@@ -0,0 +1,12 @@
+target("TestResponseParser")
+  set_kind("binary")
+  add_files("./*.cpp")
+  add_deps("WeaselIPC", "WeaselIPCServer")
+  add_rules("subcmd")
+  before_build(function(target)
+    local target_dir = path.join(target:targetdir(), target:name())
+    if not os.exists(target_dir) then
+      os.mkdir(target_dir)
+    end
+    target:set("targetdir", target_dir)
+  end)
