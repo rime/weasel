235308dc7425529b49ffe3a5eb29947a4657f8cd
居戎氏 <chen.sst@gmail.com>
2025-01-02 11:44:47 +0800

    chore(install_boost): update boost download url

--- install_boost.bat
+++ install_boost.bat
@@ -10,7 +10,7 @@ if not defined BOOST_ROOT set BOOST_ROOT=%RIME_ROOT%\deps\boost_%boost_x_y_z%
 if exist "%BOOST_ROOT%\boost" goto boost_found
 for %%I in ("%BOOST_ROOT%\.") do set src_dir=%%~dpI
 rem download boost source
-aria2c https://boostorg.jfrog.io/artifactory/main/release/%boost_version%/source/boost_%boost_x_y_z%.7z -d %src_dir%
+aria2c https://archives.boost.io/release/%boost_version%/source/boost_%boost_x_y_z%.7z -d %src_dir%
 pushd %src_dir%
 7z x boost_%boost_x_y_z%.7z
 popd
