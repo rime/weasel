45e9db0160fa8a2f98041a0f6207b619c3a9d01f
fxliang <fx.liang@outlook.com>
2026-02-11 17:57:44 +0800

    fix(WeaselUI): mark metrics of fullscreenlayout

--- WeaselUI/FullScreenLayout.cpp
+++ WeaselUI/FullScreenLayout.cpp
@@ -24,30 +24,11 @@ void weasel::FullScreenLayout::DoLayout(CDCHandle dc) {
   int step = 32;
   do {
     m_layout->DoLayout(dc);
-    if ((_style.hilited_mark_color & 0xff000000)) {
-      CSize sg;
-      if (candidates_count) {
-        if (_style.mark_text.empty())
-          GetTextSizeDW(L"|", 1, pDWR_->pTextFormat, &sg);
-        else
-          GetTextSizeDW(_style.mark_text, _style.mark_text.length(),
-                        pDWR_->pTextFormat, &sg);
-      }
-      mark_width = sg.cx;
-      mark_height = sg.cy;
-      if (_style.mark_text.empty()) {
-        mark_width = mark_height / 7;
-        if (_style.linespacing && _style.baseline)
-          mark_width =
-              (int)((float)mark_width / ((float)_style.linespacing / 100.0f));
-        mark_width = max(mark_width, 6);
-      }
-      mark_gap = (_style.mark_text.empty())
-                     ? mark_width
-                     : mark_width + _style.hilite_spacing;
-    }
   } while (AdjustFontPoint(dc, workArea, step));
 
+  mark_height = m_layout->mark_height;
+  mark_width = m_layout->mark_width;
+  mark_gap = m_layout->mark_gap;
   int offsetx = (workArea.Width() - m_layout->GetContentSize().cx) / 2;
   int offsety = (workArea.Height() - m_layout->GetContentSize().cy) / 2;
   _preeditRect = m_layout->GetPreeditRect();
